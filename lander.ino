3458bbdcbf7477524e573697fd07a17b4954aa61
Seán Hogan <78881733+seanhogan01@users.noreply.github.com>
2022-03-22 19:57:38 +0000

    Add files via upload

---
+++ lander.ino
@@ -0,0 +1,60 @@
+#include <Wire.h>
+#include <SPI.h>
+#include <SoftwareSerial.h>
+#include "Adafruit_Sensor.h"
+#include "Adafruit_BMP280.h"
+#include "ADXL335.h"
+// Function Declarations
+void bmpSetup();
+void hc12Setup();
+void adxlFunc(); // Acceleromometer
+void bmpFunc();  // Temperature / Pressure (Purple Variant)
+void hc12Func(); // Wireless Reciever
+// Naming and Variable Declarations
+ADXL335 accelerometer;
+Adafruit_BMP280 bmp; 
+SoftwareSerial HC12(7, 6); // TX Pin, RX Pin
+const int xInput = A1, yInput = A2, zInput = A3;
+String comms;
+void setup() {  // Calls Sensor Setup Functions
+  bmpSetup();
+  hc12Setup();
+  accelerometer.begin();
+} 
+void loop() {   // Calls Sensor Loop Functions
+  adxlFunc();   // Gathers and Stores accelerometer data
+  bmpFunc();    // Gathers and Stores Pressure, Temperature and Altlitude data
+  hc12Func();   // Prints wirelessly to Serial plotter
+  delay(100);
+}
+// Sensor Setup Functions
+void hc12Setup() {
+  Serial.begin(115200);
+  HC12.begin(115200);   // Serial port to HC12  
+}
+void bmpSetup() {
+  bmp.begin();          //Begin the sensor
+  while ( !Serial ) delay(100);   // wait for USB
+  bmp.setSampling(Adafruit_BMP280::MODE_NORMAL,     // Operating Mode
+                  Adafruit_BMP280::SAMPLING_X2,     // Temperature Oversampling
+                  Adafruit_BMP280::SAMPLING_X16,    // Pressure Oversampling
+                  Adafruit_BMP280::FILTER_X16,      // Filtering
+                  Adafruit_BMP280::STANDBY_MS_500); // Standby Time
+}
+// Sensor Loop Functions
+void adxlFunc() {
+  float ax,ay,az;
+  accelerometer.getAcceleration(&ax,&ay,&az); // Writes Accelerometer data to variables
+  comms = comms + String(ax-2.05) + "," + String(ay-2.09) + "," + String (az-3.32) + ",";
+}
+void bmpFunc() {
+  float pressure, temperature, altimeter;
+  pressure = bmp.readPressure();          // Reads Pressure
+  temperature = bmp.readTemperature();    // Reads Temperature
+  altimeter = bmp.readAltitude (1013.25); // Finds Alitude
+  comms = comms + String(pressure) + "," + String(temperature) + "," + String (altimeter + 63) + ",";
+}
+void hc12Func() {
+  HC12.println(comms); // Print String Comms.
+  comms = "\0";        // Wipes String Comms.
+}
